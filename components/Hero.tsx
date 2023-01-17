02c528aab37ebdaf8ac471a818985639d311cb3f
jaivardhan-bhola <jaivardhan.bhola@gmail.com>
2023-01-17 12:15:15 +0530

    Updated Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -26,7 +26,7 @@ export default function Hero(): ReactElement {
   return (
     <div>
       <h1 className="sr-only">
-        Hello I'm Jaivardhan Bhola, I'm a software engineer, and I love building things for the web.
+        Hello I'm Jaivardhan Bhola, I build desktop and web apps.
       </h1>
       <motion.div
         className="relative z-10 flex h-[calc(100vh-81px)] md:h-[calc(100vh-116px)] items-center"
